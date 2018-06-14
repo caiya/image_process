7f7dec730c957bdbd9e56606ceeae91859b75c4c
caiya <caiya928@aliyun.com>
2018-06-14 20:25:27 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fastify = require('fastify')()
 
-const concat = require('concat-stream')
 const fs = require('fs')
 const pump = require('pump')
 
@@ -12,32 +11,19 @@ fastify.get('/', async (request, reply) => {
 
 fastify.post('/upload', function (req, reply) {
 
-  const mp = req.multipart(handler, function (err) {
+  const mp = req.multipart((field, file, filename, encoding, mimetype) => {
+
+    pump(file, fs.createWriteStream('./public/uploads/' + filename))
+
+  }, err => {
     console.log('upload completed')
     reply.code(200).send()
   })
-
-  // mp is an instance of
-  // https://www.npmjs.com/package/busboy
 
   mp.on('field', function (key, value) {
     console.log('form-data', key, value)
   })
 
-  function handler (field, file, filename, encoding, mimetype) {
-    // to accumulate the file in memory! Be careful!
-    //
-    // file.pipe(concat(function (buf) {
-    //   console.log('received', filename, 'size', buf.length)
-    // }))
-    //
-    // or
-
-    pump(file, fs.createWriteStream('./public/uploads/1.jpg'))
-
-    // be careful of permission issues on disk and not overwrite
-    // sensitive files that could cause security risks
-  }
 })
 
 const start = async () => {
